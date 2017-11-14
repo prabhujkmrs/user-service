f17f40f726e20c97681de68709d829c37c619b9d
Andrei Brabete <abrabete@and.digital>
2017-11-14 16:32:48 +0000

    Added docker compose to travis 2

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,14 +1,6 @@
-IMAGE_NAME=$1
-CONTAINER_NAME=$RANDOM
+#!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-    echo "Must provide 1 argument: ./healtcheck.sh IMAGE_NAME"
-    exit 1
-fi
-
-echo Running container $CONTAINER_NAME using image $IMAGE_NAME
-docker run -it -d --rm --name ${CONTAINER_NAME} ${IMAGE_NAME}
-
+CONTAINER_NAME=$(docker-compose ps -q user-service)
 echo Running healthcheck...
 for i in 1 2 3 4 5 6 7 8 9 10; do
     [[ "healthy" = "$(docker inspect -f '{{.State.Health.Status}}' $CONTAINER_NAME)" ]] && echo "Healthcheck passed" && break
@@ -16,6 +8,4 @@ for i in 1 2 3 4 5 6 7 8 9 10; do
     sleep 6
 done
 
-echo Stopping container...
-docker stop $CONTAINER_NAME
 exit 0
